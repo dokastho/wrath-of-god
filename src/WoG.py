6215cc401c84ba24d84ef4825c5fa5285c6af00a
Thomas Dokas <dokastho@umich.edu>
2022-05-13 05:48:53 +0000

    rough draft lol

--- src/WoG.py
+++ src/WoG.py
@@ -1,11 +1,12 @@
 """Class file for WoG
 dokastho@umich.edu"""
 
+from hb import hb
+import json
 import logging
 import os
-from threading import Thread
-from hb import hb
 import socket
+from threading import Thread
 
 logging.basicConfig(filename='wog.log', encoding='utf-8', level=logging.DEBUG)
 LOGGER = logging.getLogger(__name__)
@@ -15,8 +16,12 @@ class WoG:
     def __init__(self, host, port, hb_port):
         """Construct the WoG socket and listen for messages from hb thread."""
 
-        self.sock = socket.socket()
+        # init the socket
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((host, port))
+
+        # get port number
         self.port = self.sock.getsockname()[1]
 
         # log start
@@ -31,13 +36,56 @@ class WoG:
         self.signals = {"shutdown": False}
 
         # instantiate hb obj
-        hbstart = hb(self.host, self.hb_port, self.signals)
+        self.hb_obj = hb(self.host, self.hb_port, self.signals)
 
         # start thread to listen for heartbeats
-        heartbeatThread = Thread(
-            target=hbstart.connection,
+        self.hb_thread = Thread(
+            target=self.hb_obj.connection,
             args=())
-        heartbeatThread.start()
+        self.hb_thread.start()
 
-    def listen():
+    def listen(self):
         """Listen for messages from hb thread"""
+
+        # start socket
+        self.sock.listen()
+
+        # listen on socket for client connection file descriptors
+        while True:
+            try:
+                connectionfd, address = self.sock.accept()
+            except socket.timeout:
+                continue
+            # debug info
+            LOGGER.info("Connection from", address[0])
+            with connectionfd:
+                msg_chunks = []
+                while True:
+                    try:
+                        data = connectionfd.recv(4096)
+                    except socket.timeout:
+                        continue
+                    if not data:
+                        break
+                    msg_chunks.append(data)
+            # Decode list-of-byte-strings to UTF8 and parse JSON data
+            msg_bytes = b''.join(msg_chunks)
+            msg_str = msg_bytes.decode("utf-8")
+            try:
+                message_dict = json.loads(msg_str)
+            except json.JSONDecodeError:
+                continue
+            
+            msg_type = message_dict["message_type"]
+            if msg_type == "dropped":
+                # connection dropped
+                pass
+            if msg_type == "reconnect":
+                # attempting new connection
+                pass
+            if msg_type == "fail":
+                # connection attempt failed
+                pass
+            if msg_type == "lost":
+                # reconnections won't work
+                pass
--- src/hb.py
+++ src/hb.py
@@ -1,9 +1,114 @@
-"""Class file for heartbeat thread"""
+"""Class file for heartbeat thread."""
+
+import json
+import os
+import socket
+from threading import Thread
+import time
+import socket
+import struct
+import select
+import random
+import asyncore
+
+
+ICMP_ECHO_REQUEST = 8
+
 
 class hb:
-    def __init__(self, host, port, signals):
+    def __init__(self, host, port, dest, signals):
         """Initialize heartbeat socket."""
+
+        # init the socket
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((host, port))
+
+        # get port number
+        self.port = self.sock.getsockname()[1]
+        self.host = host
+        self.port = port
+        self.signals = signals
         
-    def connection():
+        self.timeout = 1
+        
+        self.dest = dest
+
+    def connection(self):
         """Maintain connection with google nameservers."""
-    
+
+        # start socket
+        self.sock.listen()
+
+        # listen on socket for client connection file descriptors
+        while True:
+            self.send_one()
+            try:
+                connectionfd, address = self.sock.accept()
+            except socket.timeout:
+                continue
+            with connectionfd:
+                msg_chunks = []
+                while True:
+                    try:
+                        data = connectionfd.recv(4096)
+                    except socket.timeout:
+                        continue
+                    if not data:
+                        break
+                    msg_chunks.append(data)
+            # Decode list-of-byte-strings to UTF8 and parse JSON data
+            msg_bytes = b''.join(msg_chunks)
+            msg_str = msg_bytes.decode("utf-8")
+            try:
+                message_dict = json.loads(msg_str)
+            except json.JSONDecodeError:
+                continue
+
+            msg_type = message_dict["message_type"]
+
+    def send_one(self):
+        """Send one packet."""
+        # send ping
+        packet_id = int((id(self.timeout) * random.random()) / 65535)
+        
+        pack = create_packet(packet_id)
+
+        self.sock.sendto(pack, (self.dest, 1))
+
+
+# https://gist.github.com/pklaus/856268
+def checksum(source_string):
+    # I'm not too confident that this is right but testing seems to
+    # suggest that it gives the same answers as in_cksum in ping.c.
+    sum = 0
+    count_to = (len(source_string) / 2) * 2
+    count = 0
+    while count < count_to:
+        this_val = ord(source_string[count + 1]) * \
+            256+ord(source_string[count])
+        sum = sum + this_val
+        count = count + 2
+    if count_to < len(source_string):
+        sum = sum + ord(source_string[len(source_string) - 1])
+    sum = (sum >> 16) + (sum & 0xffff)
+    sum = sum + (sum >> 16)
+    answer = ~sum
+    answer = answer & 0xffff
+    # Swap bytes
+    answer = answer >> 8 | (answer << 8 & 0xff00)
+    return answer
+
+
+def create_packet(id):
+    """Create a new echo request packet based on the given "id"."""
+    # Header is type (8), code (8), checksum (16), id (16), sequence (16)
+    header = struct.pack('bbHHh', ICMP_ECHO_REQUEST, 0, 0, id, 1)
+    data = 192 * 'Q'
+    # Calculate the checksum on the data and the dummy header.
+    my_checksum = checksum(header + data)
+    # Now that we have the right checksum, we put that in. It's just easier
+    # to make up a new header than to stuff it into the dummy.
+    header = struct.pack('bbHHh', ICMP_ECHO_REQUEST, 0,
+                         socket.htons(my_checksum), id, 1)
+    return header + data
