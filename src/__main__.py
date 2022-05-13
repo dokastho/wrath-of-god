05d59de3e449b8a2561c601b1900efcf9a520282
Thomas Dokas <dokastho@umich.edu>
2022-05-13 06:00:49 +0000

    add to script

--- src/__main__.py
+++ src/__main__.py
@@ -3,9 +3,13 @@
 import os
 from WoG import WoG
 
+
 def main():
     """Main script for WoG"""
     host = "localhost"
     port = 0  # let os decide
-    
-    retribution = WoG()
+    dest = "google.com"
+    hb_port = 10582
+
+    retribution = WoG(host, port, dest, hb_port)
+    retribution.listen()
